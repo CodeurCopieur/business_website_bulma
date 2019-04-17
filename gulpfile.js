8ce4f746c926174f67010670f66b5f576fbd0622
codwerk <louis_widdy@live.fr>
2019-04-18 00:02:25 +0200

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@ const gulp = require('gulp');
       sass = require('gulp-sass');
       concat = require('gulp-concat');
       del = require('del');
-      minify = require('gulp-clean-css')
       browserSync = require('browser-sync');
 
 const paths = {
@@ -23,8 +22,6 @@ function css(){
     return gulp.src(paths.css.src)
     //passer ce fichier par le compilateur sass
     .pipe(sass())
-    //va nous permettre de compresser nos styles CSS
-    .pipe(minify())
     //permet de concaténer tout notre code source dans un seul fichier
     .pipe(concat('index.css'))
     //Où puis-je sauvegarder le scss compilé ?
